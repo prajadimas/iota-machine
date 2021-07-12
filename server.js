d5f7ed7fd7238a1691363c3e93e36f74ebc56199
prajadimas <dmspraja2105@gmail.com>
2021-07-12 20:18:38 +0700

    catch error on cron schedule

--- server.js
+++ server.js
@@ -63,10 +63,6 @@ app.use(function (req, res, next) {
 // Error handler
 app.use(function (err, req, res, next) {
 	var customMessage = {
-		400: 'Please Comeback with Another Request',
-		404: 'Please Try Another Route, Example: /analyzer',
-		405: 'Please Try Another REST HTTP Method',
-		409: 'Please Try Another Input Data, Already Exists',
 		500: 'Please Try Again Later'
 	}
 	// set locals, only providing error in development
@@ -85,7 +81,7 @@ app.use(function (err, req, res, next) {
 })
 
 io.on('connection', (socket) => {
-  log(`Client connected on ${socket.id}}`)
+  log(`Client connected on ${socket.id}`)
   // console.log('Client connected.')
   io.to(socket.id).emit('socket id', {
     id: socket.id
@@ -95,55 +91,59 @@ io.on('connection', (socket) => {
     io.emit('timer count', data)
   })
   socket.on('disconnect', () => {
-    log(`Client disconnected on ${socket.id}}`)
+    log(`Client disconnected on ${socket.id}`)
     // console.log('Client disconnected.')
   })
 })
 
 cron.schedule('0 */1 * * * *', async () => {
-  log(`Running every 1 minute`)
-  // console.log('running every 30 seconds')
-  // console.log(io)
-  var m1runner = await machineBalanceChecker(1)
-  log(`Machine 1 ${JSON.stringify(m1runner).toString()}`)
-  // console.log(m1runner)
-  var m2runner = await machineBalanceChecker(2)
-  log(`Machine 2 ${JSON.stringify(m2runner).toString()}`)
-  // console.log(m2runner)
-  var m3runner = await machineBalanceChecker(3)
-  log(`Machine 3 ${JSON.stringify(m3runner).toString()}`)
-  // console.log(m3runner)
-  var m4runner = await machineBalanceChecker(4)
-  log(`Machine 4 ${JSON.stringify(m4runner).toString()}`)
-  // console.log(m4runner)
-  var m5runner = await machineBalanceChecker(5)
-  log(`Machine 5 ${JSON.stringify(m5runner).toString()}`)
-  // console.log(m5runner)
-  if (m1runner.d > 0) {
-    io.emit('m1bal', {
-      bal: m1runner.d
-    })
-  }
-  if (m2runner.d > 0) {
-    io.emit('m2bal', {
-      bal: m2runner.d
-    })
-  }
-  if (m3runner.d > 0) {
-    io.emit('m3bal', {
-      bal: m3runner.d
-    })
-  }
-  if (m4runner.d > 0) {
-    io.emit('m4bal', {
-      bal: m4runner.d
-    })
-  }
-  if (m5runner.d > 0) {
-    io.emit('m5bal', {
-      bal: m5runner.d
-    })
-  }
+	try {
+		log(`Running every 1 minute`)
+	  // console.log('running every 30 seconds')
+	  // console.log(io)
+	  var m1runner = await machineBalanceChecker(1)
+	  log(`Machine 1 ${JSON.stringify(m1runner).toString()}`)
+	  // console.log(m1runner)
+	  var m2runner = await machineBalanceChecker(2)
+	  log(`Machine 2 ${JSON.stringify(m2runner).toString()}`)
+	  // console.log(m2runner)
+	  var m3runner = await machineBalanceChecker(3)
+	  log(`Machine 3 ${JSON.stringify(m3runner).toString()}`)
+	  // console.log(m3runner)
+	  var m4runner = await machineBalanceChecker(4)
+	  log(`Machine 4 ${JSON.stringify(m4runner).toString()}`)
+	  // console.log(m4runner)
+	  var m5runner = await machineBalanceChecker(5)
+	  log(`Machine 5 ${JSON.stringify(m5runner).toString()}`)
+	  // console.log(m5runner)
+	  if (m1runner.d > 0) {
+	    io.emit('m1bal', {
+	      bal: m1runner.d
+	    })
+	  }
+	  if (m2runner.d > 0) {
+	    io.emit('m2bal', {
+	      bal: m2runner.d
+	    })
+	  }
+	  if (m3runner.d > 0) {
+	    io.emit('m3bal', {
+	      bal: m3runner.d
+	    })
+	  }
+	  if (m4runner.d > 0) {
+	    io.emit('m4bal', {
+	      bal: m4runner.d
+	    })
+	  }
+	  if (m5runner.d > 0) {
+	    io.emit('m5bal', {
+	      bal: m5runner.d
+	    })
+	  }
+	} catch (err) {
+		log(`Error: ${JSON.stringify(err).toString()}`)
+	}
 })
 
 // Export our app for another purposes
